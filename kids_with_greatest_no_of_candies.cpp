2781e8b982f3a1b742e82c23b64be594c69d3f79
Rupam Dutta <67874782+dtrup00@users.noreply.github.com>
2024-05-31 11:53:12 +0530

    Add files via upload

---
+++ kids_with_greatest_no_of_candies.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> boolAns;
+        int maxCandies = 0;
+        for(int i=0; i<candies.size(); i++) {
+            if(candies[i] > maxCandies) maxCandies = candies[i];
+        }
+        for(int i=0; i<candies.size(); i++) {
+            if(candies[i] + extraCandies >= maxCandies) boolAns.push_back(true);
+            else boolAns.push_back(false);
+        }
+        return boolAns;
+    }
+};
